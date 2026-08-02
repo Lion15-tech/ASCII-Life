9a4d0ef4d9cb7afb63c79accbadba1f4da7d9daa
Lion15-tech <25422070060424@cecyteq.edu.mx>
2026-08-02 17:16:06 -0600

    Se añadió la lista de objetos

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ import os
 
 #Definimos el mapa como una lista de listas 
 mapa = [
-    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
-    ["#", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
-    ["#", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
-    ["#", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
-    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
+    # 0    1    2    3    4    5    6    7    8    9
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"], # 0
+    ["#", ".", ".", ".", ".", ".", ".", ".", ".", "#"], # 1
+    ["#", ".", ".", ".", ".", ".", ".", ".", ".", "#"], # 2
+    ["#", ".", ".", ".", ".", ".", ".", ".", ".", "#"], # 3
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]  # 4
 ]
+objetos = [
+    {
+        "tipo": "arbol",
+        "simbolo": "T",
+        "x": 4,
+        "y": 1
+    },
+    {
+        "tipo": "cofre",
+        "simbolo": "C",
+        "x": 1,
+        "y": 1
+    }
+]
+
 
 #Coordenadas del jugador
 jugador_x = 4  
@@ -19,21 +35,31 @@ def limpiar_pantalla():
     else:
         os.system('clear')
 
-def dibujar_mapa(mapa, jugador_x, jugador_y): 
-    #                                                 así tenemos "y"    
-    #Para cada fila del mapa                                  0         # o .
-    for y, fila in enumerate(mapa): #el enumerate nos da el indice y el valor de la lista
+def dibujar_mapa(mapa, jugador_x, jugador_y, objetos): 
+    #Para cada fila del mapa
+    for y, fila in enumerate(mapa): 
         #Por cada casilla de la fila
-        for x, casilla in enumerate(fila):
-            #Si esta casilla es donde esta el jugador 
+        for x, casilla in enumerate(fila):   
+            #Si la casilla es la del jugador, lo dibujamos primero
             if x == jugador_x and y == jugador_y:
-                #Dibujamos el jugador
-                print("@", end="") #El end="" es para que no haga un salto de linea
-            else:
-                #Dibujamos el terreno normal
+                print("@", end="")
+                continue # Saltamos a la siguiente casilla para no dibujar nada más aquí
+            
+            objeto_encontrado = False
+
+            #Revisamos todos los objetos de la lista
+            for objeto in objetos:
+                if objeto["x"] == x and objeto["y"] == y:
+                    print(objeto["simbolo"], end="")
+                    objeto_encontrado = True
+                    break 
+
+            #Si revisamos todos los objetos y NO encontramos ninguno, dibujamos el terreno normal
+            if not objeto_encontrado:
                 print(casilla, end="")
 
         print()
+
 
 def puede_mover(mapa, nueva_x, nueva_y):
     casilla = mapa[nueva_y][nueva_x]
@@ -45,7 +71,7 @@ def puede_mover(mapa, nueva_x, nueva_y):
 
 while True:
     limpiar_pantalla()
-    dibujar_mapa(mapa, jugador_x, jugador_y)
+    dibujar_mapa(mapa, jugador_x, jugador_y, objetos)
 
     nueva_x = jugador_x
     nueva_y = jugador_y
